663f8313f745c49b808537f61d62e364d0d003e6
liuyong <xin_nian13@163.com>
2025-10-13 16:29:22 +0800

    Revert "CypressDemoDialog - 回退到最初版本重新开发画图功能"
    This reverts commit d999ab8169869ab26eff95339d9738ed3c5101ef.

--- CypressDemoDialog/Dialog/resource.h
+++ CypressDemoDialog/Dialog/resource.h
@@ -13,58 +13,12 @@
 #define IDC_COMBO_OUT_ENDPOINTS         1002
 #define IDC_EDIT1                       1003
 #define IDC_EDIT_OUT_COMMAND            1003
-#define IDC_EDIT_TRIG_VALUE             1003
-#define IDC_EDIT2                       1004
-#define IDC_EDIT_IN_AMP_DETECT          1004
-#define IDC_EDIT_QUERY_RESULT           1004
 #define IDC_BUTTON1                     1007
-#define IDC_BUTTON_OUT                  1007
-#define IDC_BUTTON_VERSION              1007
-#define IDC_BUTTON_STATUS               1007
 #define IDC_BUTTON2                     1008
 #define IDC_BUTTON_IN                   1008
 #define IDC_BUTTON_ADC_SAMPLE           1008
 #define IDC_CUSTOM1                     1010
 #define IDC_CUSTOM_SHOW                 1010
-#define IDC_BUTTON_TRIGGER              1011
-#define IDC_STATIC_AMP                  1012
-#define IDC_BUTTON3                     1013
-#define IDC_BUTTON_IMPEDANCE            1013
-#define IDC_EDIT3                       1014
-#define IDC_EDIT_IMPEDANCE              1014
-#define IDC_BUTTON4                     1015
-#define IDC_BUTTON_SET_SAMPLE_FREQ      1015
-#define IDC_EDIT4                       1016
-#define IDC_EDIT_SAMPLE_FREQ            1016
-#define IDC_BUTTON6                     1018
-#define IDC_BUTTON_DL_NUM               1018
-#define IDC_BUTTON_TOTAL_DL_NUM         1018
-#define IDC_BUTTON7                     1019
-#define IDC_BUTTON_SAMPLE_FREQ          1019
-#define IDC_BUTTON8                     1020
-#define IDC_BUTTON_AD_FREQ              1020
-#define IDC_BUTTON9                     1021
-#define IDC_BUTTON_TRIG_LENGTH          1021
-#define IDC_BUTTON10                    1022
-#define IDC_BUTTON_MAX_FREQ             1022
-#define IDC_STATIC_RESULT               1023
-#define IDC_BUTTON_MAX_INC              1024
-#define IDC_BUTTON_MAX_DEC              1025
-#define IDC_BUTTON_MIN_INC              1026
-#define IDC_BUTTON_MIN_DEC              1027
-#define IDC_STATIC_UART                 1028
-#define IDC_EDIT_PRODUCT_ID             1030
-#define IDC_BUTTON11                    1031
-#define IDC_BUTTON_SEND                 1031
-#define IDC_COMBO_PORT_Nr               1033
-#define IDC_BUTTON_UART_TRIG            1034
-#define IDC_EDIT5                       1035
-#define IDC_EDIT_UART_TRIG              1035
-#define IDC_CHECK_TOTAL_DL_NUM          1036
-#define IDC_CHECK_18_DL_NUM             1036
-#define IDC_CHECK_IMPEDANCE             1037
-#define IDC_EDIT6                       1038
-#define IDC_EDIT_ANALYSIS               1038
 
 // Next default values for new objects
 // 
@@ -72,7 +26,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1039
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
